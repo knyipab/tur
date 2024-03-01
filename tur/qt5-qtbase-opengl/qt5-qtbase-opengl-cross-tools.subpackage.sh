8f0d79da1ee6491a6dbe461230aeb6501940ee95
Ronald Y <knyipab@gmail.com>
2024-03-01 15:21:37 +0000

    shotcut

--- tur/qt5-qtbase-opengl/qt5-qtbase-opengl-cross-tools.subpackage.sh
+++ tur/qt5-qtbase-opengl/qt5-qtbase-opengl-cross-tools.subpackage.sh
@@ -1,5 +1,7 @@
 TERMUX_SUBPKG_DESCRIPTION="Tools for cross build on the host (NOT for Termux)"
 TERMUX_SUBPKG_DEPENDS="qt5-qtbase-opengl"
+TERMUX_SUBPKG_CONFLICTS="qt5-qtbase-cross-tools"
+TERMUX_SUBPKG_REPLACES="qt5-qtbase-cross-tools"
 TERMUX_SUBPKG_INCLUDE="
 opt/qt/cross/bin/*
 opt/qt/cross/lib/*
