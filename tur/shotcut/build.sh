a27b87175d0e83329efe5c5860fdeb23fb3202ef
Ronald Y <knyipab@gmail.com>
2024-05-28 09:02:42 +0800

    add build depends

--- tur/shotcut/build.sh
+++ tur/shotcut/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION=24.04.28
 TERMUX_PKG_SRCURL=https://github.com/mltframework/shotcut/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=cc40aeb8b7f50fb8e102f45e1accbc4b4153f14b565154a4f4a6ec446f5591e4
 TERMUX_PKG_DEPENDS="mlt, qt6-qtbase, qt6-qtcharts, qt6-qtdeclarative, qt6-qtimageformats, qt6-qtmultimedia, qt6-qttranslations, fftw, frei0r-plugins, sdl, sdl2"
+TERMUX_PKG_BUILD_DEPENDS="qt6-qttools"
 TERMUX_PKG_SUGGESTS="ffmpeg, libexif, libsoxr, sox, rubberband, libsamplerate"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
