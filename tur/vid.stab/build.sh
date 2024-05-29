678abd1237418fdfc767b0a36c8e36da7b176c04
Ronald Y <knyipab@gmail.com>
2024-05-30 00:59:01 +0800

    test

--- tur/vid.stab/build.sh
+++ tur/vid.stab/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://public.hronopik.de/vid.stab
 TERMUX_PKG_DESCRIPTION="Video stabilization library."
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_LICENSE="GPL"
+TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.1.1
 TERMUX_PKG_SRCURL=git+https://github.com/georgmartius/vid.stab.git
 TERMUX_PKG_AUTO_UPDATE=true
