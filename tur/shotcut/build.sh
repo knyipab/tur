926c8ac40a23b2921e47b6a4fec1975e443f2348
Ronald Y <knyipab@gmail.com>
2024-05-29 21:15:09 +0800

    test

--- tur/shotcut/build.sh
+++ tur/shotcut/build.sh
@@ -5,8 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=24.04.28
 TERMUX_PKG_SRCURL=https://github.com/mltframework/shotcut/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=cc40aeb8b7f50fb8e102f45e1accbc4b4153f14b565154a4f4a6ec446f5591e4
-# TODO: movit
-TERMUX_PKG_DEPENDS="ffmpeg, fftw, frei0r-plugins, mlt, libx264, libvpx, lame, ladspa-sdk, qt6-qtbase, qt6-qtcharts, qt6-qtdeclarative, qt6-qtimageformats, qt6-qtmultimedia, qt6-qttranslations, sdl, sdl2"
+TERMUX_PKG_DEPENDS="ffmpeg, fftw, frei0r-plugins, mlt, movit, libx264, libvpx, lame, ladspa-sdk, qt6-qtbase, qt6-qtcharts, qt6-qtdeclarative, qt6-qtimageformats, qt6-qtmultimedia, qt6-qttranslations, sdl, sdl2"
 TERMUX_PKG_BUILD_DEPENDS="qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BLACKLISTED_ARCHES="i686, arm"
