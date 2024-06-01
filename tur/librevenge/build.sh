ed5d1946d91fec535c1925df9e52a8f1e174a360
Ronald Y <knyipab@gmail.com>
2024-06-01 23:08:04 +0800

    librevenge attempt

--- tur/librevenge/build.sh
+++ tur/librevenge/build.sh
@@ -7,3 +7,31 @@ TERMUX_PKG_SRCURL=https://sourceforge.net/projects/libwpd/files/librevenge/libre
 TERMUX_PKG_SHA256=106d0c44bb6408b1348b9e0465666fa83b816177665a22cd017e886c1aaeeb34
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost, cppunit, doxygen"
+
+ termux_step_pre_configure() { 
+ 	## fix other archs hidden bugs
+    ## aarch64
+    # SKIP: ./lib/librevenge-0.0.a
+    # ERROR: ./lib/librevenge-0.0.so contains undefined symbols:
+    #      5: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND __aarch64_ldadd8_relax
+    #     27: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND __aarch64_ldadd8_acq_rel
+    #     46: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND __aarch64_ldadd4_acq_rel
+    # SKIP: ./lib/librevenge-generators-0.0.a
+    # ERROR: ./lib/librevenge-generators-0.0.so contains undefined symbols:
+    #    127: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND __aarch64_ldadd8_acq_rel
+    # SKIP: ./lib/librevenge-stream-0.0.a
+    ## arm
+    # ERROR: ./lib/libcppunit-1.15.so contains undefined symbols:
+    #     51: 00000000     0 NOTYPE  GLOBAL DEFAULT   UND __aeabi_uidiv
+    #     60: 00000000     0 NOTYPE  GLOBAL DEFAULT   UND __aeabi_idiv
+    # SKIP: ./lib/libcppunit.a
+    ## i686
+    # SKIP: ./lib/librevenge-0.0.a
+    # ERROR: ./lib/librevenge-0.0.so contains undefined symbols:
+    #     44: 00000000     0 NOTYPE  GLOBAL DEFAULT   UND __umoddi3
+    # SKIP: ./lib/librevenge-generators-0.0.a
+    # SKIP: ./lib/librevenge-stream-0.0.a
+    # ERROR: Refer above
+ 	LDFLAGS+=" $($CC -print-libgcc-file-name)" 
+ 	autoreconf -fi 
+ } 
