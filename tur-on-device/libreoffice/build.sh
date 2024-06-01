44544425af89dff7335617d03e1da8878eb3a73a
Ronald Y. <46513942+knyipab@users.noreply.github.com>
2024-06-01 09:16:28 +0000

    disable nss

--- tur-on-device/libreoffice/build.sh
+++ tur-on-device/libreoffice/build.sh
@@ -11,7 +11,10 @@ TERMUX_PKG_SHA256=f6e2059cd85d07793e8d95828b2412906bdba8bf61a5f76b8c51907898481e
 # TODO: to be added
 TERMUX_PKG_DEPENDS="cups, openjdk-17, python, which"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-java"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--without-java
+--disable-nss
+"
 
 termux_step_pre_configure() {
 	sed -i -e 's/linux-gnu\*|k\*bsd\*-gnu\*|linux-musl\*)/linux-*|k*bsd*-gnu*)/' configure.ac
