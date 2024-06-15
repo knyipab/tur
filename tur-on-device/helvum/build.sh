b843373a5fb89fa74cc5c42e1f83d4e5e3186b09
Ronald Y <knyipab@gmail.com>
2024-06-15 14:18:10 +0800

    helvum retry

--- tur-on-device/helvum/build.sh
+++ tur-on-device/helvum/build.sh
@@ -2,12 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.freedesktop.org/pipewire/helvum
 TERMUX_PKG_DESCRIPTION="A GTK patchbay for pipewire. "
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.5.1"
-TERMUX_PKG_SRCURL="https://gitlab.freedesktop.org/pipewire/helvum/-/archive/${TERMUX_PKG_VERSION}/helvum-${TERMUX_PKG_VERSION}.tar.bz2"
-TERMUX_PKG_SHA256=d4f5cc0c3a70a91edfc816f12a10426dadd9ca74ea82662e2df5e6c4eb31d8ca
+TERMUX_PKG_VERSION="0.0.1"
+TERMUX_PKG_SRCURL=git+https://gitlab.freedesktop.org/pipewire/helvum/
+TERMUX_PKG_GIT_BRANCH="main"
+# TERMUX_PKG_VERSION="0.5.1"
+# TERMUX_PKG_SRCURL="https://gitlab.freedesktop.org/pipewire/helvum/-/archive/${TERMUX_PKG_VERSION}/helvum-${TERMUX_PKG_VERSION}.tar.bz2"
+# TERMUX_PKG_SHA256=d4f5cc0c3a70a91edfc816f12a10426dadd9ca74ea82662e2df5e6c4eb31d8ca
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, graphene, gtk4, hicolor-icon-theme, libadwaita, libcairo, pipewire, pango"
 TERMUX_PKG_BUILD_DEPENDS="appstream-glib"
-TERMUX_PKG_AUTO_UPDATE=true
+# TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	if [ "${TERMUX_ON_DEVICE_BUILD}" = false ]; then
