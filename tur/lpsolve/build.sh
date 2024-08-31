3669aed60f7ee5c056f1136d88fce4c48372a6c1
Ronald Y <knyipab@gmail.com>
2024-08-31 11:17:35 +0800

    lpsolve

--- tur/lpsolve/build.sh
+++ tur/lpsolve/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://lpsolve.sourceforge.net/
-TERMUX_PKG_DESCRIPTION="Library that provides a high-level interface to RDF data"
+TERMUX_PKG_DESCRIPTION="a Mixed Integer Linear Programming (MILP) solver"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0, BSD"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=5.5.2.11
@@ -7,3 +7,7 @@ TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/lpsolve/lpsolve/${TE
 TERMUX_PKG_SHA256=6d4abff5cc6aaa933ae8e6c17a226df0fc0b671c438f69715d41d09fe81f902f
 
 # TODO: detect Archlinux update
+
+termux_step_pre_configure() {
+	chmod +x configure
+}
