981c9bb47be23fdf08af91d161220609bf1ed72c
Ronald Y <knyipab@gmail.com>
2024-08-31 23:10:07 +0800

    lpsolve

--- tur/lpsolve/build.sh
+++ tur/lpsolve/build.sh
@@ -9,9 +9,9 @@ TERMUX_PKG_BUILD_IN_SRC=true
 
 # TODO: detect Archlinux update
 
-termux_step_configure() {
-	patch -Np1 -i "$TERMUX_PKG_BUILDER_DIR"/lpsolve-5.5.2.11-misc.patch
-}
+# termux_step_configure() {
+# 	patch -Np1 -i "$TERMUX_PKG_BUILDER_DIR"/lpsolve-5.5.2.11-misc.patch
+# }
 
 termux_step_make() {
 	cd lpsolve55
