7e0e6f3a7827d47c20a7fd095bfe37ce8a385e74
Ronald Y <knyipab@gmail.com>
2024-08-31 11:38:00 +0800

    lpsolve

--- tur/lpsolve/build.sh
+++ tur/lpsolve/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=5.5.2.11
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/lpsolve/lpsolve/${TERMUX_PKG_VERSION}/lp_solve_${TERMUX_PKG_VERSION}_source.tar.gz
 TERMUX_PKG_SHA256=6d4abff5cc6aaa933ae8e6c17a226df0fc0b671c438f69715d41d09fe81f902f
+TERMUX_PKG_BUILD_IN_SRC=true
 
 # TODO: detect Archlinux update
 
