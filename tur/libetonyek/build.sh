8c32522309844d87c8cb041d0e8e3a46df643d1b
Ronald Y <knyipab@gmail.com>
2024-08-31 22:08:00 +0800

    libetonyek

---
+++ tur/libetonyek/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=https://wiki.documentfoundation.org/DLP/Libraries/libetonyek
+TERMUX_PKG_DESCRIPTION="Library and a set of tools for reading and converting Apple Keynote presentations"
+TERMUX_PKG_LICENSE="MPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION=0.1.10
+TERMUX_PKG_SRCURL=https://dev-www.libreoffice.org/src/libetonyek/libetonyek-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=b430435a6e8487888b761dc848b7981626eb814884963ffe25eb26a139301e9a
+TERMUX_PKG_DEPENDS="libxml2, libwpd, librevenge, liblangtag, zlib"
+TERMUX_PKG_BUILD_DEPENDS="boost, cppunit, glm"
+
+
+# TODO: detect Archlinux update
