0f8f3b8f9def6e55ae3badcb0dd4507132c85f20
Ronald Y <knyipab@gmail.com>
2024-08-31 23:12:37 +0800

    mdds print dir

--- tur/mdds/build.sh
+++ tur/mdds/build.sh
@@ -6,3 +6,7 @@ TERMUX_PKG_VERSION=2.1.1
 TERMUX_PKG_SRCURL=https://gitlab.com/api/v4/projects/mdds%2Fmdds/packages/generic/source/${TERMUX_PKG_VERSION}/mdds-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=1483d90cefb8aa4563c4d0a85cb7b243aa95217d235d422e9ca6722fd5b97e56
 TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	ls -la .
+}
