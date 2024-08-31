8cac8f512eaf8c25e292552087fba7a7234b31a1
Ronald Y <knyipab@gmail.com>
2024-08-31 10:20:37 +0800

    cppunit retry

--- tur/cppunit/build.sh
+++ tur/cppunit/build.sh
@@ -7,3 +7,7 @@ TERMUX_PKG_SRCURL=https://dev-www.libreoffice.org/src/cppunit-$TERMUX_PKG_VERSIO
 TERMUX_PKG_SHA256=89c5c6665337f56fd2db36bc3805a5619709d51fb136e51937072f63fcc717a7
 
 # TODO: detect Archlinux update
+
+termux_step_pre_configure() {
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+}
