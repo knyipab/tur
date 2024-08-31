f980c639f8bbc4186a0f3c1fc5b79f50c2df80f8
Ronald Y <knyipab@gmail.com>
2024-08-31 11:02:12 +0800

    libvisio

--- tur/libvisio/build.sh
+++ tur/libvisio/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.documentfoundation.org/DLP/Libraries/libvisio
 TERMUX_PKG_DESCRIPTION="Library providing ability to interpret and import visio diagrams"
-TERMUX_PKG_LICENSE="LGPL"
+TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.1.7
 TERMUX_PKG_SRCURL=https://dev-www.libreoffice.org/src/libvisio/libvisio-${TERMUX_PKG_VERSION}.tar.xz
