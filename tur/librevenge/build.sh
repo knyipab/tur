2db9f08c175cd3acf0fcdb41a7a0dd13e1d82f89
Ronald Y <knyipab@gmail.com>
2024-08-31 10:40:20 +0800

    librevenge without cppunit

--- tur/librevenge/build.sh
+++ tur/librevenge/build.sh
@@ -6,8 +6,7 @@ TERMUX_PKG_VERSION=0.0.5
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/libwpd/files/librevenge/librevenge-${TERMUX_PKG_VERSION}/librevenge-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=106d0c44bb6408b1348b9e0465666fa83b816177665a22cd017e886c1aaeeb34
 TERMUX_PKG_DEPENDS="zlib"
-TERMUX_PKG_BUILD_DEPENDS="boost, cppunit"
-
+TERMUX_PKG_BUILD_DEPENDS="boost"
 
 # TODO: detect Archlinux update
 
