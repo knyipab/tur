b4ebc8f39c633d320a854ba58989c6bd1880465c
Ronald Y <knyipab@gmail.com>
2024-09-01 18:56:46 +0800

    libexttextcat

--- tur/libexttextcat/build.sh
+++ tur/libexttextcat/build.sh
@@ -5,6 +5,4 @@ TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=3.4.7
 TERMUX_PKG_SRCURL=https://dev-www.libreoffice.org/src/libexttextcat-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=df0a59d413a5b202573d8d4f5159e33a8538da4f8e8e60979facc64d6290cebd
-TERMUX_PKG_DEPENDS=""
-TERMUX_PKG_BUILD_DEPENDS=""
 # TODO: detect Archlinux update
