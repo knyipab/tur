ed4bfc963e1e9092e974b24f18e038c33862f91e
Ronald Y <knyipab@gmail.com>
2024-09-01 20:25:24 +0800

    liborcus

--- tur/liborcus/build.sh
+++ tur/liborcus/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE="https://gitlab.com/orcus/orcus/blob/master/README.md"
+TERMUX_PKG_HOMEPAGE="https://gitlab.com/orcus/orcus"
 TERMUX_PKG_DESCRIPTION="File import filter library for spreadsheet documents."
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.19.2
-TERMUX_PKG_SRCURL=https://kohei.us/files/orcus/src/liborcus-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=69ed26a00d4aaa7688e62a6e003cbc81928521a45e96605e53365aa499719e39
+TERMUX_PKG_SRCURL=https://gitlab.com/orcus/orcus/-/archive/${TERMUX_PKG_VERSION}/orcus-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4fb632224aecc29e79c432f862c446b32f97b81d6855fa3773a2f11eda3d1c3b
 TERMUX_PKG_DEPENDS="boost, libixion, python, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost, mdds"
 # TODO: detect Archlinux update
