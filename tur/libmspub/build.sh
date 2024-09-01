9187423623ae1f44244fdf47195fc6a0013a169f
Ronald Y <knyipab@gmail.com>
2024-09-01 14:45:37 +0800

    libcdr, libmspub, libodfgen, libwpg

--- tur/libmspub/build.sh
+++ tur/libmspub/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.1.4
 TERMUX_PKG_SRCURL=https://dev-www.libreoffice.org/src/libmspub/libmspub-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=ef36c1a1aabb2ba3b0bedaaafe717bf4480be2ba8de6f3894be5fd3702b013ba
-TERMUX_PKG_DEPENDS="libwpd, libicu, librevenge, zlib"
+TERMUX_PKG_DEPENDS="libwpd, libicu, libxml2, librevenge, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost, libwpg"
 
 # TODO: detect Archlinux update
--- tur/libwpg/build.sh
+++ tur/libwpg/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://libwpg.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Library for importing and converting Corel WordPerfect(tm) Graphics images."
-TERMUX_PKG_LICENSE="LGPL-2.0, MPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=0.1.8
-TERMUX_PKG_SRCURL=https://sourceforge.net/projects/libwpd/files/libodfgen/libodfgen-${TERMUX_PKG_VERSION}/libodfgen-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=55200027fd46623b9bdddd38d275e7452d1b0ff8aeddcad6f9ae6dc25f610625
+TERMUX_PKG_VERSION=0.3.4
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/libwpg/libwpg-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=b55fda9440d1e070630eb2487d8b8697cf412c214a27caee9df69cec7c004de3
 TERMUX_PKG_DEPENDS="libwpd, perl, librevenge"
 TERMUX_PKG_BUILD_DEPENDS="boost"
 
