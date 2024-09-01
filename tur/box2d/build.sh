fd605c915ab3bd32314646304970865d956b70ee
Ronald Y <knyipab@gmail.com>
2024-09-01 21:31:57 +0800

    box2d

--- tur/box2d/build.sh
+++ tur/box2d/build.sh
@@ -7,3 +7,7 @@ TERMUX_PKG_SRCURL="https://github.com/erincatto/Box2D/archive/v${TERMUX_PKG_VERS
 TERMUX_PKG_SHA256=64ad759006cd2377c99367f51fb36942b57f0e9ad690ed41548dd620e6f6c8b1
 TERMUX_PKG_BUILD_DEPENDS="git, glfw, libxcursor, libxinerama, libxkbcommon, libxt, xorg-xrandr"
 TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -lglfw -lGL"
+}
