bf5f304083c9a667f5918104051eecd71316f267
Ronald Y <knyipab@gmail.com>
2024-09-01 19:51:11 +0800

    libixion

--- tur/libixion/build.sh
+++ tur/libixion/build.sh
@@ -1,11 +1,11 @@
-TERMUX_PKG_HOMEPAGE="https://gitlab.com/ixion/ixion/blob/master/README.md"
+TERMUX_PKG_HOMEPAGE="https://gitlab.com/ixion/ixion"
 TERMUX_PKG_DESCRIPTION="A general purpose formula parser & interpreter"
-TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.19.0
-TERMUX_PKG_SRCURL="https://kohei.us/files/ixion/src/libixion-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=b4864d7a55351a09adbe9be44e5c65b1d417e80e946c947951d0e8428b9dcd15
+TERMUX_PKG_SRCURL="https://gitlab.com/ixion/ixion/-/archive/${TERMUX_PKG_VERSION}/ixion-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=b5b67ea7fc631a0fda4fff3123f0cc2e3831849bdd8fbae8443be0766a77f243
 TERMUX_PKG_DEPENDS="boost, python"
 TERMUX_PKG_BUILD_DEPENDS="boost, mdds, libspdlog"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static"
-# TODO: detect Archlinux update
+TERMUX_PKG_AUTO_UPDATE=true
