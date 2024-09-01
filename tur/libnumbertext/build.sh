73ef20967cdcaa32adb1121c4670a0b596439109
Ronald Y <knyipab@gmail.com>
2024-09-01 20:30:05 +0800

    libnumbertext

--- tur/libnumbertext/build.sh
+++ tur/libnumbertext/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE="https://github.com/Numbertext/libnumbertext"
 TERMUX_PKG_DESCRIPTION="Number to number name and money text conversion library"
-TERMUX_PKG_LICENSE="LGPL-3.0, LGPL-3.0"
+TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=1.0.11
 TERMUX_PKG_SRCURL=https://github.com/Numbertext/libnumbertext/archive/${TERMUX_PKG_VERSION}.tar.gz
