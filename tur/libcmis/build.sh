eb56d40a62ab4e2efa03a70fdfd32622667334aa
Ronald Y <knyipab@gmail.com>
2024-09-01 20:58:37 +0800

    libcmis

---
+++ tur/libcmis/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/tdf/libcmis"
+TERMUX_PKG_DESCRIPTION="a C/C++ client library for the CMIS protocol"
+TERMUX_PKG_LICENSE="GPL2, LGPL2.1, MPL"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION=0.6.2
+TERMUX_PKG_SRCURL="https://github.com/tdf/libcmis/releases/download/v${TERMUX_PKG_VERSION}/libcmis-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=1b5c2d7258ff93eb5f9958ff0e4dfd7332dc75a071bb717dde2217a26602a644
+TERMUX_PKG_DEPENDS="boost, curl, libxml2"
+TERMUX_PKG_BUILD_DEPENDS="boost, cppunit, docbook2x"
+TERMUX_PKG_AUTO_UPDATE=true
