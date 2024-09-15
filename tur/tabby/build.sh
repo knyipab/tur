31a093f0f72a59b4f08fab3f95ea9e0d45bae4f7
Ronald Y <knyipab@gmail.com>
2024-09-15 12:39:19 +0800

    tabby retry i686

--- tur/tabby/build.sh
+++ tur/tabby/build.sh
@@ -9,7 +9,7 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 # simd-json v0.13.10 fail to compile for i686 with error related to avx2 instructions
-TERMUX_PKG_BLACKLISTED_ARCHES="i686"
+# TERMUX_PKG_BLACKLISTED_ARCHES="i686"
 
 termux_step_pre_configure() {
 	termux_setup_rust
