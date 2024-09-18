fff464373c2b53ae65b5d0c1da853a510c0ecf2f
Ronald Y <knyipab@gmail.com>
2024-09-18 11:40:04 +0800

    libreoffice fix

--- tur-on-device/libreoffice-on-device/build.sh
+++ tur-on-device/libreoffice-on-device/build.sh
@@ -7,11 +7,10 @@ _LIBREOFFICE_VERSION=24.2.3.2
 TERMUX_PKG_SRCURL=https://download.documentfoundation.org/libreoffice/src/$TERMUX_PKG_VERSION/libreoffice-$_LIBREOFFICE_VERSION.tar.xz
 TERMUX_PKG_SHA256=f6e2059cd85d07793e8d95828b2412906bdba8bf61a5f76b8c51907898481e64
 # Ref: https://gitlab.archlinux.org/archlinux/packaging/packages/libreoffice-fresh/-/blob/main/PKGBUILD?ref_type=heads
-# TODO: to be added compared to Archlinux deps="libwpd, libwps, neon, nspr, redland, lpsolve, gcc-libs, sh, libvisio, libetonyek, libodfgen, libcdr, libmspub, nss, clucene, libpagemaker, libabw, libmwaw, libe-book, liblangtag, libexttextcat, liborcus, libcmis, libzmf, libnumbertext, libfreehand, libstaroffice, libepubgen, libqxp, box2d, expat, glib2, glibc, librevenge"
 # TODO/FIXME: xdg-utils is unsafe for on device build
 # TODO: add back qt6 after qmake6 are available
-TERMUX_PKG_DEPENDS="which, bison, hunspell, python, pango, libjpeg-turbo, libxrandr, libhyphen, libgraphite, libicu, libxslt, libglvnd, poppler, harfbuzz-icu, hicolor-icon-theme, desktop-file-utils, shared-mime-info, libxinerama, cups, littlecms, libwebp, libtommath, libatomic-ops, xmlsec, gpgme, libepoxy, libzxing-cpp, fontconfig, openldap, zlib, libpng, freetype, libraptor2, libxml2, libcairo, libx11, boost, libtiff, libxext, openjpeg, dbus, glm, openssl, argon2, curl, libcurl"
-TERMUX_PKG_BUILD_DEPENDS="gtk4, gtk3, qt5-qtbase, qt5-qmake, qt5-qtx11extras, postgresql, unixodbc, mariadb, libc++"
+TERMUX_PKG_DEPENDS="mdds, which, bison, hunspell, python, pango, libjpeg-turbo, libxrandr, libhyphen, libgraphite, libicu, libxslt, libglvnd, poppler, harfbuzz-icu, hicolor-icon-theme, desktop-file-utils, shared-mime-info, libxinerama, cups, littlecms, libwebp, libtommath, libatomic-ops, xmlsec, gpgme, libepoxy, libzxing-cpp, fontconfig, openldap, zlib, libpng, freetype, libraptor2, libxml2, libcairo, libx11, boost, libtiff, libxext, openjpeg, dbus, glm, openssl, argon2, curl, libcurl, libwpd, libwps, libneon, libnspr, redland, lpsolve, libvisio, libetonyek, libodfgen, libcdr, libmspub, libnss, clucene, libpagemaker, libabw, libmwaw, libe-book, liblangtag, libexttextcat, liborcus, libcmis, libzmf, libnumbertext, libfreehand, libstaroffice, libepubgen, libqxp, box2d, libexpat, librevenge"
+TERMUX_PKG_BUILD_DEPENDS="cppunit, gtk4, gtk3, qt5-qtbase, qt5-qmake, qt5-qtx11extras, qt6-qtbase, qt6-qtmultimedia, postgresql, unixodbc, mariadb, libc++"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --host=$TERMUX_ARCH-linux
@@ -19,52 +18,51 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-sdremote-bluetooth
 
 --without-java
---disable-nss
 --without-webdav
 --disable-curl
---disable-libcmis
---without-system-clucene
---without-system-xmlsec
+--with-system-libcmis
+--with-system-clucene
+--with-system-xmlsec
 
---without-system-librevenge
---without-system-libodfgen
---without-system-libepubgen
---without-system-libvisio
---without-system-libwpd
---without-system-libwpg
---without-system-libwps
---without-system-libcdr
---without-system-libmspub
---without-system-libmwaw
---without-system-libetonyek
---without-system-libfreehand
---without-system-libebook
---without-system-libabw
---without-system-libpagemaker
---without-system-libqxp
---without-system-libzmf
---without-system-libstaroffice
+--with-system-librevenge
+--with-system-libodfgen
+--with-system-libepubgen
+--with-system-libvisio
+--with-system-libwpd
+--with-system-libwpg
+--with-system-libwps
+--with-system-libcdr
+--with-system-libmspub
+--with-system-libmwaw
+--with-system-libetonyek
+--with-system-libfreehand
+--with-system-libebook
+--with-system-libabw
+--with-system-libpagemaker
+--with-system-libqxp
+--with-system-libzmf
+--with-system-libstaroffice
 
---without-system-cppunit
+--with-system-cppunit
 
---without-system-libtommath
---without-system-mdds
+--with-system-libtommath
+--with-system-mdds
 --without-system-dragonbox
 --without-system-frozen
 --without-system-libfixmath
 --without-system-hsqldb
 --without-system-sane
---without-system-orcus
---without-system-redland
---without-system-box2d
+--with-system-orcus
+--with-system-redland
+--with-system-box2d
 --without-system-zxcvbn
---without-system-libexttextcat
+--with-system-libexttextcat
 --without-system-mythes
---without-system-libnumbertext
+--with-system-libnumbertext
 --disable-gstreamer-1-0
---without-system-liblangtag
+--with-system-liblangtag
 
---disable-lpsolve
+--enable-ext-nlpsolver
 --disable-coinmp
 --enable-dbus
 --enable-gtk3
@@ -95,8 +93,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_pre_configure() {
 	# sed -i -e 's/linux-gnu\*|k\*bsd\*-gnu\*|linux-musl\*)/linux-*|k*bsd*-gnu*)/' configure.ac
 	# sed -i -e 's/linux-gnu\*|k\*bsd\*-gnu\*|linux-musl\*)/linux-*|k*bsd*-gnu*)/' configure
-	export QT6DIR=$TERMUX_PREFIX/lib/qt6
-	sed -i "109d" $TERMUX_PKG_SRCDIR/i18npool/source/breakiterator/data/sent.txt
+	# export QT6DIR=$TERMUX_PREFIX/lib/qt6
+	export qt6_libexec_dirs=$TERMUX_PREFIX/lib/qt6
+	# sed -i "109d" $TERMUX_PKG_SRCDIR/i18npool/source/breakiterator/data/sent.txt
 	## fix error: <cstdlib> tried including <stdlib.h>
 	# CFLAGS+=" -Wno-implicit-function-declaration"
 	# CPLUS_INCLUDE_PATH+=" -I$TERMUX_PREFIX/include/"
